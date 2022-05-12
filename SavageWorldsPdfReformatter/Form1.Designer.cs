aea6e3d9948217c401f7e33e672e6e18eab01e36
Nelviticus <github@voidpuppy.com>
2022-05-12 17:58:37 +0100

    Initial

---
+++ SavageWorldsPdfReformatter/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SavageWorldsPdfReformatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(385, 426);
+            this.richTextBoxInput.TabIndex = 0;
+            this.richTextBoxInput.Text = "";
+            this.richTextBoxInput.TextChanged += new System.EventHandler(this.richTextBoxInput_TextChanged);
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxOutput.Location = new System.Drawing.Point(403, 12);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(385, 426);
+            this.richTextBoxOutput.TabIndex = 1;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.richTextBoxInput);
+            this.Name = "Form1";
+            this.Text = "Savage Worlds PDF Reformatter";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+    }
+}
+
---
+++ SavageWorldsPdfReformatter/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SavageWorldsPdfReformatter
+{
+    public partial class Form1 : Form
+    {
+        const char Apostrophe = '’';
+        const char ApostropheReplacement = '\'';
+        const string Hyphen = "—";
+        const string HyphenReplacement = " - ";
+        const char Dash = '-';
+        const char DashReplacement = '-';
+        const string Bullet = "• ";
+        const string BulletReplacement = "\r•\t";
+        const string DoubleLineBreak = "\n\n";
+        const string DoubleLineBreakPlaceholder = "_DOUBLE_LINEBREAK_PLACEHOLDER_";
+        const string LineBreak = "\n";
+        const string LineBreakReplacement = " ";
+        const string SpaceAtEndOfLine = " \r";
+        const string SpaceAtStartOfLine = "\r ";
+        const string NewLineBreak = "\r";
+
+        const int WidthOffset = 49;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            ReformatText();
+            CopyOutputToClipboard();
+            richTextBoxInput.SelectAll();
+        }
+
+        private void CopyOutputToClipboard()
+        {
+            if (richTextBoxOutput.Text != string.Empty)
+            {
+                Clipboard.SetText(richTextBoxOutput.Text);
+            }
+        }
+
+        private void ReformatText()
+        {
+            string text = richTextBoxInput.Text;
+            string reformattedText = text
+                .Replace(Apostrophe, ApostropheReplacement)
+                .Replace(Hyphen, HyphenReplacement)
+                .Replace(Dash, DashReplacement)
+                .Replace(Bullet, BulletReplacement)
+                .Replace(DoubleLineBreak, DoubleLineBreakPlaceholder)
+                .Replace(LineBreak, LineBreakReplacement)
+                .Replace(DoubleLineBreakPlaceholder, NewLineBreak)
+                .Replace(SpaceAtEndOfLine, NewLineBreak)
+                .Replace(SpaceAtStartOfLine, NewLineBreak)
+                .Replace("armor", "armour")
+                .Replace("Armor", "Armour")
+                .Replace("color", "colour")
+                .Replace("Color", "Colour")
+                .Replace("odor", "odour")
+                .Replace("Odor", "Odour")
+                .Trim();
+            richTextBoxOutput.Text = Regex.Replace(reformattedText, @"[ ]{2,}", " ");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeTextBoxes();
+        }
+
+        private void ResizeTextBoxes()
+        {
+            int formWidth = this.Width;
+            int textBoxWidth = (formWidth - WidthOffset) / 2;
+            int outbutBoxOriginalWidth = richTextBoxOutput.Width;
+            richTextBoxInput.Width = textBoxWidth;
+            richTextBoxOutput.Width = textBoxWidth;
+            richTextBoxOutput.Location = new Point(richTextBoxOutput.Location.X - textBoxWidth + outbutBoxOriginalWidth, richTextBoxOutput.Location.Y);
+        }
+    }
+}
